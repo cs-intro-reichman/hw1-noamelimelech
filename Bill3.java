1f4df6a3b9ee51bd542c5a7d338e44080c97b4f1
Noam Elimelech <noam.elimelech01@post.runi.ac.il>
2025-11-01 17:30:15 +0200

    fix print format

--- Bill3.java
+++ Bill3.java
@@ -7,7 +7,7 @@ public class Bill3 {
 		String name3 = args [2];
 		int check = Integer.parseInt(args[3]);
 		double each = Math.ceil(check / 3.0);
-		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ":  pay" + " " + each + " " + "Shekels each");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay" + " " + each + " " + "Shekels each");
 	    // Replace this comment with the rest of your code   
 	}
 }
